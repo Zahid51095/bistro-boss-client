7a5f2fcd414db2cc2ab80448de58c56b0312f308
Zahid Hasan <zahidhasan140338@gmail.com>
2023-11-15 13:58:06 +0600

    used custom hook to load data

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg"
+
 
 const Menu = () => {
   return (
@@ -6,8 +9,8 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-
-      <h2>hfdskhsdgk</h2>
+      <Cover img={menuImg} title="our menu"></Cover>
+      
     </div>
   );
 };
