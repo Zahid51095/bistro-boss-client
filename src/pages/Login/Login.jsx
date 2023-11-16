9885f73b98c0cbcd0d47aa883f1019060e122a95
Zahid Hasan <zahidhasan140338@gmail.com>
2023-11-16 22:52:10 +0600

    private route has been set

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate,  validateCaptcha } from 'react-simple-captcha';
 import { AuthContext } from '../../providers/AuthProvider';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2';
 
@@ -12,6 +12,12 @@ const Login = () => {
     const [disabled, setDisabled] = useState(true);
 
     const {signIn} = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+    console.log(from);
 
     useEffect( () =>{
         loadCaptchaEnginge(6); 
@@ -44,6 +50,8 @@ const Login = () => {
                   `
                 }
               });
+              console.log(from);
+              navigate(from, { replace: true })
         })
     }
 
