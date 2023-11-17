46746d9b6f4c505ee860dda3e6b5885f533794c0
Zahid Hasan <zahidhasan140338@gmail.com>
2023-11-17 22:44:37 +0600

    tanstack query has been applied successfully

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,8 @@ import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import Secret from "../pages/Shared/Secrets/Secret";
 import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../Layout/Dashboard";
+import Cart from "../pages/Dashboard/Cart/Cart";
 
   
  export const router = createBrowserRouter([
@@ -43,4 +45,15 @@ import PrivateRoute from "./PrivateRoute";
         }
       ]
     },
+    {
+      path: 'dashboard',
+      element: <Dashboard></Dashboard>,
+      children: [
+        {
+          path: 'cart',
+          element: <Cart></Cart>
+
+        }
+      ] 
+    }
   ]);
