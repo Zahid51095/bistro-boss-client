fab3c45fc3a4b422c5d527faaf7f98f1ce6f508e
Zahid Hasan <zahidhasan140338@gmail.com>
2023-11-19 01:15:15 +0600

    axios interceptor has been addedd and lots of hook has been used

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -10,6 +11,8 @@ const AuthProvider = ({children}) => {
     const [loading, setLoading] = useState(true);
 
     const googleProvider = new GoogleAuthProvider();
+
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -41,13 +44,26 @@ const AuthProvider = ({children}) => {
     useEffect( () => {
        const unsubscribe =  onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
-            console.log('current user', currentUser);
+            if(currentUser){
+                // get token and store client
+                const userInfo = { email: currentUser.email } 
+                axiosPublic.post('/jwt', userInfo)
+                .then(res =>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+            }
+            else{
+                // TODO: remove token (if token stored in the client side: local storage, caching, in memory)
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         });
         return () => {
             return unsubscribe();
         }
-    },[])
+    },[axiosPublic])
 
     const authInfo = {
         user,
